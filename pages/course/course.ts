887b4951197434acf7a32837c69b1fc6aedddae5
azurity <784227594@qq.com>
2019-02-10 20:57:38 +0800

    消除一些bug

--- pages/course/course.ts
+++ pages/course/course.ts
@@ -1,16 +1,13 @@
-const app = getApp()
+// import { IMyApp } from '../../app'
+
+// const app = getApp<IMyApp>()
+
 Page({
     data: {
-        StatusBar: app.globalData.StatusBar,
-        CustomBar: app.globalData.CustomBar,
         TabCur: 0,
         scrollLeft: 0
     },
-    tabSelect(e) {
+    tabSelect(e: wx.TapEvent) {
         console.log(e)
-        this.setData({
-            TabCur: e.currentTarget.dataset.id,
-            scrollLeft: (e.currentTarget.dataset.id - 1) * 60
-        })
     }
 })
