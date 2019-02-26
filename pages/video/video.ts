0fe22896cf2443a61e37d082e316c1d426b87cf5
azurity <784227594@qq.com>
2019-02-26 22:28:54 +0800

    2019年2月26日22:28:50

--- pages/video/video.ts
+++ pages/video/video.ts
@@ -28,16 +28,20 @@ Page({
                 color: '#ff00ff',
                 time: 3
             }
-        ],
-        videoContext: null
+        ]
     },
     onLoad() {
-        let coursename = wx.getStorageSync('name')
-        let ur = wx.getStorageSync('url')
-        this.setData({
-            courseName: coursename,
-            url: ur
-        })
+        try {
+            let coursename = wx.getStorageSync('name')
+            let ur = wx.getStorageSync('url')
+            this.setData({
+                courseName: coursename,
+                url: ur // 可用测试视频'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'
+            })
+            wx.setStorageSync('lastTime', 0)
+        } catch (e) {
+            //TODO
+        }
     },
     bindInputBlur(e: any) {
         this.inputValue = e.detail.value
@@ -47,5 +51,17 @@ Page({
             text: this.inputValue,
             color: getRandomColor()
         })
+    },
+    timeUpdate(e: wx.CustomEvent<'timeupdate', { currentTime: number; duration: number }>) {
+        let lastTime = wx.getStorageSync('lastTime')
+        if (Math.abs(e.detail.currentTime - lastTime) > 2) {
+            // 防拖动
+            this.videoContext.seek(lastTime)
+            return
+        }
+        wx.setStorageSync('lastTime', e.detail.currentTime)
+    },
+    finish() {
+        console.log('finish')
     }
 })
